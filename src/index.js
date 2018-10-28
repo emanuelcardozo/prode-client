25cd49232be72959324e583ae2920614f78e6527
maxie <maxiperezc@gmail.com>
2018-10-28 16:37:34 -0300

    Adding https redirect

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import config from 'config/config'
 import { Provider } from 'react-redux'
 import { Router } from 'react-router-dom'
 import store, { history } from './config/store'
+import HttpsRedirect from 'react-https-redirect'
 import 'assets/css/material-dashboard-react.css?v=1.4.1'
 import registerServiceWorker from './registerServiceWorker'
 
@@ -12,9 +13,12 @@ if( config.raven ) window.Raven.config(config.raven).install()
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history}>
-      <App />
-    </Router>
+    <HttpsRedirect>
+      <Router history={history}>
+        <App />
+      </Router>
+    </HttpsRedirect>
+
   </Provider>,
   document.getElementById('root')
 )
