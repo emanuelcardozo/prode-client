f4f6d6c0fd6fd5989f68a3034b499ec94e428fa9
maxie <maxiperezc@gmail.com>
2018-11-30 18:42:38 -0300

    config del orto

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,14 +1,12 @@
 const config = {
   development: {
-    api: 'http://localhost:3001',
-  },
-  test: {
-    raven: 'https://0665e172df0442888b2dde9a9630af11@sentry.io/1306597'
-  },
-  production: {
     raven: 'https://0665e172df0442888b2dde9a9630af11@sentry.io/1306597',
     api: 'https://prodevolution-api.herokuapp.com'
-  }
+  },
+  
+  // development: {
+  //   api: 'http://localhost:3001',
+  // }
 }
 
 export default config[process.env.NODE_ENV] // eslint-disable-line
