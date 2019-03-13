bcf63cb4d30ff82cbb835e942bb98cb5242d9a23
maxie <maxiperezc@gmail.com>
2019-03-13 18:15:43 -0300

    Adding new rules to tab Puntajes

--- src/views/Ayuda/Puntajes.jsx
+++ src/views/Ayuda/Puntajes.jsx
@@ -11,7 +11,19 @@ class Puntajes extends React.Component {
           <strong>* 1 punto</strong> por acertar los goles del local.<br />
           <strong>* 1 punto</strong> por acertar los goles del visitante.<br /></p>
         <p>Cada partido se puede modificar hasta el minuto anterior a que empieza.</p>
-        <p>Durante la fecha podrás ver como cambia partido a partido la tabla de posiciones y saber si sos el campeón de la fecha.</p>
+        <p>Durante la fecha podrás ver como cambia partido a partido la tabla de posiciones y saber si sos el campeón de la fecha.</p><br />
+        <p>En el caso del modo <strong>Partido</strong>, los puntos adquiridos dependen de:<br />
+          <strong>* 1 punto</strong> por acertar Local, Empate o Visitante.<br />
+          <strong>* 1 punto</strong> por acertar los goles del local.<br />
+          <strong>* 1 punto</strong> por acertar los goles del visitante.<br />
+          <strong>* 1 punto</strong> por acertar quien realiza la primer falta.<br />
+          <strong>* 1 punto</strong> por acertar quien obtiene la primer amarilla.<br />
+          <strong>* 1 punto</strong> por acertar quien realiza el primer lateral.<br />
+          <strong>* 1 punto</strong> por acertar quien realiza el primer corner.<br />
+          <strong>* 1 punto</strong> por acertar quien realiza el primer disparo al arco.<br />
+          <strong>* 1 punto</strong> por acertar quien realiza el primer offside.<br />
+        </p>
+        <p><strong>**Para guardar una predicción se deben seleccionar todas las categorias**</strong></p>
       </div>
     )
   }
